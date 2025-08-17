fdcf15dfbcabe252d634d7b60725c71a08517c4e
Sebastian Coetzee <seb.coetzee@gmail.com>
2025-08-17 22:48:22 +0100

    Remove build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VERSION=$(awk -F'"' '/version = / {print $2}' Cargo.toml | head -n 1)
-TAG="registry.sebastiancoetzee.com/expenses:$VERSION"
-
-docker build -t $TAG .
-docker image push $TAG
